bd321e2ee7873f058f2903f1cfa992a12aca9d02
Mirzohid22 <smm2226fstd@gmail.com>
2024-07-11 14:25:28 +0500

    added: Banner & Footer coponents to all pages

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Navigation from "@/components/Navigation";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,16 +12,20 @@ export default function Contact() {
       className={`min-h-screen flex flex-col items-center justify-start ${inter.className}`}
     >
       <Navigation />
-      <div className="w-screen flex items-center justify-center">contact page</div>
+      <Footer />
+      <div className="w-full max-w-[var(--max-width)] flex justify-between font-medium text-[15px] leading-[18.15px] text-[#858585] py-8">
+        <p>© Company All Rights Reserved by Romi Agency</p>
+        <p>Terms & Conditions Legal Notice</p>
+      </div>
     </main>
   );
 }
 
 export async function getStaticProps({ locale }: { locale: string }) {
-    return {
-      props: {
-        ...(await serverSideTranslations(locale, ["common"])),
-        // Will be passed to the page component as props
-      },
-    };
-  }
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+      // Will be passed to the page component as props
+    },
+  };
+}
--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,26 +1,39 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 import Navigation from "@/components/Navigation";
+import Banner from "@/components/common/Banner";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Products() {
+  const { t } = useTranslation("common");
   return (
     <main
-      className={`min-h-screen flex flex-col items-center justify-start ${inter.className}`}
+      className={`min-h-screen flex flex-col items-center justify-start ${inter.className} gap-5`}
     >
       <Navigation />
-      <div className="w-screen flex items-center justify-center">Products page</div>
+      <Banner
+        variant="blue"
+        title={t("Welcome.title")}
+        description={t("Welcome.description")}
+      />
+      <Footer />
+      <div className="w-full max-w-[var(--max-width)] flex justify-between font-medium text-[15px] leading-[18.15px] text-[#858585] py-8">
+        <p>© Company All Rights Reserved by Romi Agency</p>
+        <p>Terms & Conditions Legal Notice</p>
+      </div>
     </main>
   );
 }
 
 export async function getStaticProps({ locale }: { locale: string }) {
-    return {
-      props: {
-        ...(await serverSideTranslations(locale, ["common"])),
-        // Will be passed to the page component as props
-      },
-    };
-  }
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+      // Will be passed to the page component as props
+    },
+  };
+}
