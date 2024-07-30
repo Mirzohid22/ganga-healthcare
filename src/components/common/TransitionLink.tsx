0bd6b57e07566dec4f0faed1841136d1dff36e64
Mirzohid22 <smm2226fstd@gmail.com>
2024-07-31 00:48:38 +0500

    added: Spinner on Transitioning between pages

--- src/components/common/TransitionLink.tsx
+++ src/components/common/TransitionLink.tsx
@@ -3,6 +3,14 @@ import Link, { type LinkProps } from "next/link";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
+const Spinner: React.FC = () => {
+  return (
+    <div className="fixed top-[50vh] left-[50vw] transform z-50">
+      <div className="w-20 h-20 border-[10px] border-gray-300 border-t-black rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
 interface TransitionLinkProps extends LinkProps {
   isButton?: boolean;
   children: React.ReactNode;
@@ -51,15 +59,54 @@ const TransitionLink: React.FC<TransitionLinkProps> = ({
     const body = document.querySelector("body");
 
     if (isTransitioning) {
-      body?.classList.add("page-transition");
+      body?.classList.add("transition");
+      body?.classList.add("transition-all");
+      body?.classList.add("smooth");
+      body?.classList.add("duration-700");
+      body?.classList.add("pt-10");
+      // create a container to overlay the page
+      const overlay = document.createElement("div");
+      overlay.setAttribute("id", "overlay");
+      overlay.classList.add(
+        "fixed",
+        "top-0",
+        "left-0",
+        "w-full",
+        "h-full",
+        "bg-black",
+        "opacity-50",
+        "z-40",
+      );
+      body?.appendChild(overlay);
+      // add SPinner to body
+      const spinner = document.createElement("div");
+      spinner.setAttribute("id", "spinner");
+
+      spinner.classList.add(
+        "fixed",
+        "top-[50vh]",
+        "left-[50vw]",
+        "transform",
+        "-translate-x-1/2",
+        "-translate-y-1/2",
+        "z-50"
+      );
+      spinner.innerHTML = `<div class="w-20 h-20 border-[10px] border-gray-300 border-t-black rounded-full animate-spin"></div>`;
+      body?.appendChild(spinner);
     } else {
-      body?.classList.remove("page-transition");
+      // remove Spinner from body
+      const spinner = document.getElementById("spinner");
+      spinner?.remove();
+      // remove Overlay from body
+      const overlay = document.getElementById("overlay");
+      overlay?.remove();
+      body?.classList.remove("pt-10")
     }
   }, [isTransitioning]);
 
   return (
     <>
-      {/* {isTransitioning && <Spinner />} */}
+      {/* {isTransitioning && <Spinner />} Add Spinner here */}
       {isButton ? (
         <Link
           className={`${centered ? "mx-auto" : ""}`}
