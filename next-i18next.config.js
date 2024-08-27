44de9f26f6fb7dd3c454793bf01eb1690199e05a
Mirzohid22 <smm2226fstd@gmail.com>
2024-08-27 22:41:01 +0500

    modified: next config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,8 +3,5 @@ const config = {
     locales: ["en", "ru"],
     defaultLocale: "en",
   },
-  images: {
-    domains: ["gangahealthcare.s3.us-east-2.amazonaws.com", "gangahealthcare.s3.amazonaws.com"],
-  },
 };
 module.exports = config;
--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,12 @@
 import config from "./next-i18next.config.js";
 const nextConfig = {
   ...config,
+  images: {
+    domains: [
+      "gangahealthcare.s3.us-east-2.amazonaws.com",
+      "gangahealthcare.s3.amazonaws.com",
+    ],
+  },
   reactStrictMode: true,
 };
 
